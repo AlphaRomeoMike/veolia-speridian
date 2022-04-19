952230bcdf0545503c8b834be9e7745ee3df7ee5
mushahidansari <mushahid.ansari@speridian.com>
2022-04-19 23:10:06 +0500

    sample data table

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalFormComponent } from '../modal-form/modal-form.component';
-import { FormControl, FormGroup, } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,10 +9,15 @@ import { FormControl, FormGroup, } from '@angular/forms';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  
-  constructor(public modalService: NgbModal) { }
+  empForm!: FormGroup;
 
-  ngOnInit(): void { }
+  constructor(public modalService: NgbModal, private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.empForm = this.fb.group({
+      employees: this.fb.array([])
+    });
+   }
 
   openModal() {
     const modalRef = this.modalService.open(ModalFormComponent);
@@ -29,5 +34,50 @@ export class DashboardComponent implements OnInit {
         }
       }
     });
+  }
+  
+  employees(): FormArray {
+    return this.empForm.get('employees') as FormArray;
+  }
+
+  newEmployee(): FormGroup {
+    return this.fb.group({
+      firstName: '',
+      lastName: '',
+      skills: this.fb.array([])
+    });
+  }
+
+  addEmployee() {
+    this.employees().push(this.newEmployee());
+  }
+
+  removeEmployee(empIndex: number) {
+    this.employees().removeAt(empIndex);
+  }
+
+  employeeSkills(empIndex: number): FormArray {
+    return this.employees()
+      .at(empIndex)
+      .get('skills') as FormArray;
+  }
+
+  newSkill(): FormGroup {
+    return this.fb.group({
+      skill: '',
+      exp: ''
+    });
+  }
+
+  addEmployeeSkill(empIndex: number) {
+    this.employeeSkills(empIndex).push(this.newSkill());
+  }
+
+  removeEmployeeSkill(empIndex: number, skillIndex: number) {
+    this.employeeSkills(empIndex).removeAt(skillIndex);
+  }
+
+  onSubmit() {
+    console.log(this.empForm.value);
   }
 }

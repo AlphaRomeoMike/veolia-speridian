da32e851cb4680211f1c241451a9d473052b58cd
mushahidansari <mushahid.ansari@speridian.com>
2022-04-20 17:48:52 +0500

    table init

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,10 +11,114 @@ import { FormArray, FormBuilder, FormControl, FormGroup, } from '@angular/forms'
 export class DashboardComponent implements OnInit, AfterViewInit {
   empForm!: FormGroup;
   providedData: any;
+  tableHeaders: any[];
+  gridData: any = [];
 
   constructor(public modalService: NgbModal, private fb: FormBuilder, private cdref: ChangeDetectorRef) { }
 
   ngOnInit(): void {
+    this.tableHeaders = [
+      {
+        text: 'Unit or Equipment Functional Description',
+        hidden: false
+      },
+      {
+        text: 'Asset Type',
+        hidden: false
+      },
+      {
+        text: 'Style',
+        hidden: false
+      },
+      {
+        text: 'Size',
+        hidden: false
+      },
+      {
+        text: 'Duty/Application',
+        hidden: false
+      },
+      {
+        text: 'Quality',
+        hidden: false
+      },
+      {
+        text: 'Qty',
+        hidden: false
+      },
+      {
+        text: 'Load %',
+        hidden: false
+      },
+      {
+        text: 'Life %',
+        hidden: false
+      },
+      {
+        text: 'Life %',
+        hidden: false
+      },
+      {
+        text: 'Unit Template Descripton',
+        hidden: true
+      },
+      {
+        text: 'Unit Template Application',
+        hidden: true
+      },
+      {
+        text: 'Units',
+        hidden: true
+      },
+      {
+        text: 'Rev.',
+        hidden: true
+      },
+      {
+        text: 'LAvg. Year',
+        hidden: true
+      },
+      {
+        text: 'Year 1',
+        hidden: true
+      },
+      {
+        text: 'Year 2',
+        hidden: true
+      },
+      {
+        text: 'Year 3',
+        hidden: true
+      },
+      {
+        text: 'Year 4',
+        hidden: true
+      },
+      {
+        text: 'Year 5',
+        hidden: true
+      },
+      {
+        text: 'Year 6',
+        hidden: true
+      },
+      {
+        text: 'Year 7',
+        hidden: true
+      },
+      {
+        text: 'Year 8',
+        hidden: true
+      },
+      {
+        text: 'Year 9',
+        hidden: true
+      },
+      {
+        text: 'Year 10',
+        hidden: true
+      }
+    ]
     this.empForm = this.fb.group({
       employees: this.fb.array([])
     });
@@ -26,35 +130,73 @@ export class DashboardComponent implements OnInit, AfterViewInit {
     console.log(this.providedData);
 
     if (this.providedData) {
-      this.addEmployee();
+      this.tableInt();
+      // this.addEmployee();
     }
+  }
+
+  tableInt() {
+    this.gridData = JSON.parse(localStorage.getItem('data')!);
+    // this.providedData.forEach(item => {
+    //   // let holdData = [];
+    //   Object.keys(item).forEach(key => {
+    //     // holdData.push(item[key]);
+    //     this.gridData.push({ text: item[key] });
+    //   });
+
+    //   // holdData.map(x => this.gridData.push({ text: x }))
+    //   // this.gridData.push(holdData.flat());
+    // });
+    this.cdref.detectChanges();
+
+    // debugger
+    // this.dataTable = this.providedData;
   }
 
   openModal() {
     const modalRef = this.modalService.open(ModalFormComponent);
     modalRef.result.then((result: any) => {
       if (result) {
-        debugger;
-        this.removeAllEmployee(this.employees());
+
+        // this.removeAllEmployee(this.employees());
         if (!localStorage.getItem('data')) {
-          result.skills = []
-          const dataArray = { employees: [result] };
+          // result.skills = []
+          // const dataArray = { employees: [result] };
 
+          const data = this.objIteration(result);
           // dataArray.employees.push(result.skills = []);
-          localStorage.setItem('data', JSON.stringify(dataArray));
-
+          localStorage.setItem('data', JSON.stringify(data));
           this.providedData = JSON.parse(localStorage.getItem('data')!);
-          this.addEmployee();
-        } else {
+
+          // this.addEmployee();
+          // this.tableInt();
+        } else { 
           const dataHolder = JSON.parse(localStorage.getItem('data')!);
-          result.skills = [];
-          dataHolder.employees.push(result);
+          // result.skills = [];
+          const data = this.objIteration(result);
+          dataHolder.push(...data);
           localStorage.setItem('data', JSON.stringify(dataHolder));
           this.providedData = JSON.parse(localStorage.getItem('data')!);
-          this.addEmployee()
+          // this.addEmployee()
+          // this.tableInt();
+
         }
       }
     });
+  }
+
+  objIteration(data): any {
+
+    let tableTr = [];
+    let tableTd = [];
+    Object.keys(data).forEach(key => {
+      // holdData.push(item[key]);
+      tableTd.push(data[key]);
+    });
+
+    tableTr.push(tableTd);
+    this.gridData.push(tableTd);
+    return tableTr;
   }
 
   employees(): FormArray {
